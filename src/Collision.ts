6e0f771ea3d2f06239497b38d3e035364af6e16d
unknown <kybra.korkmaz@gmail.com>
2026-08-02 13:58:23 +0300

    game implemented

---
+++ src/Collision.ts
@@ -0,0 +1,54 @@
+import {Paddle} from "~/sprites/Paddle";
+import {CanvasView} from "~/view/CanvasView";
+import {Ball} from "~/sprites/Ball";
+import {Brick} from "~/sprites/Brick";
+
+export class Collision{
+    isCollidingBrick(ball: Ball, brick: Brick): boolean{
+        if(
+            ball.pos.x < brick.pos.x + brick.width &&
+            ball.pos.x + ball.width > brick.pos.x &&
+            ball.pos.y < brick.pos.y + brick.height &&
+            ball.pos.y + ball.height > brick.pos.y
+        ){
+            return true;
+        }
+        return false;
+    }
+    // check ball collision with bricks
+    isCollidingBricks(ball: Ball, bricks: Brick[]):boolean{
+        let colliding = false;
+        bricks.forEach((brick, index)=>{
+            if(this.isCollidingBrick(ball, brick)){
+                ball.changeYDirection();
+                if(brick.energy === 1){
+                    bricks.splice(index, 1);
+                }else{
+                    brick.energy -=1;
+                }
+                colliding = true;
+            }
+        });
+        return colliding;
+    }
+    checkBallCollision(ball: Ball, paddle:Paddle, view: CanvasView):void{
+        // 1. check ball collision with paddle
+        if(
+            ball.pos.x + ball.width > paddle.pos.x &&
+            ball.pos.x < paddle.pos.x +paddle.width &&
+            ball.pos.y + ball.height === paddle.pos.y
+        ){
+            ball.changeYDirection();
+        }
+        // 2. check ball collision with walls
+        // Ball movement X constraints
+        if(ball.pos.x > view.canvas.width - ball.width || ball.pos.x < 0){
+            ball.changeXDirection();
+        }
+
+        // Ball movement Y constraints
+        if(ball.pos.y < 0){ // 0 + ball.height
+            ball.changeYDirection();
+        }
+    }
+}
